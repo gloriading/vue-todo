22db131b935b32b61684d7e3edce53a1423cda4b
Gloria Ding <gloria.hc.ding@gmail.com>
2019-03-03 10:36:26 -0800

    Practice Simple Components

---
+++ src/components/GoodbyeWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box1">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoodbyeWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.box1 {
+  border: 5px solid deeppink;
+  padding: 2rem;
+  background-color: antiquewhite;
+}
+h1 {
+  color: deeppink;
+}
+</style>
+
